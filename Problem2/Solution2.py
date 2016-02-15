d3581d8723de1ef3c7052b8119be9c4a7a947dd2
Daniel Wherry <wherrydaniel@gmail.com>
2016-02-14 20:51:07 -0500

    Reordered conditionals for one less step

--- Problem2/Solution2.py
+++ Problem2/Solution2.py
@@ -3,14 +3,14 @@ startingValue = 1
 nextValue = 2
 
 while True:
+    if nextValue >= 4000000:
+       break
+    
     if startingValue % 2 == 0:
        theSum += startingValue
     elif nextValue % 2 == 0:
        theSum += nextValue
   
-    if nextValue >= 4000000:
-       break
-
     startingValue += nextValue
     nextValue += startingValue
     
