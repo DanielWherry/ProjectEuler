b9de0101514cbebe40f76786077b88af0c9bfa78
Daniel Wherry <dwherry@bai.local>
2016-08-17 08:48:01 -0500

    Making prime number generator

---
+++ Problem7/Solution7.py
@@ -0,0 +1,17 @@
+primeCounter = 0
+isPrime = False
+keepLooking = True
+counter = 3
+otherCounter = 2
+
+while(keepLooking):
+	while(otherCounter < counter):
+		if(counter % otherCounter == 0)
+			break
+		elif(o)
+		otherCounter += 1
+		
+	if (primeCounter == 10001):
+		keepLooking = False
+	
+	counter += 2
